7c72bd4cadc1468e03b12fdf7aedd546efba9453
Wilson Choo <wilsonchoo@live.com>
2019-03-22 17:25:03 +0800

    Starting Lesson 4: Quiz: Practice Time

--- src/AMS.kt
+++ src/AMS.kt
@@ -9,26 +9,63 @@ fun main(args: Array<String>) {
     println(fitMoreFish(9.0, listOf(1,1,3), 3))
     println(fitMoreFish(10.0, listOf(), 7, true))*/
 
-    /*var fortune : String
-    for (i in 1..10) {
+    /*var fortune : String = ""
+    repeat (10) {
         fortune = getFortuneCookie(getBirthday())
-        println("Your fortune is $fortune")
-        if (fortune.contains("Take it easy")) break
+        println("Your Fortune is $fortune")
+        if (fortune.contains("Take it easy")) break;
     }*/
 
+    var fortune : String = ""
+    while (!fortune.contains("Take it easy")) {
+        fortune = getFortuneCookie(getBirthday())
+        println("Your fortune is $fortune")
+    }
+
     //feedTheFish()
+}
+
+// Lambda that returns a dice roll number between 1 and 12
+val rollDice = { Random().nextInt(12) + 1}
+val extendRollDice = { sides : Int -> Random().nextInt(sides) + 1}
+val rollDice0 = { sides : Int -> if (sides == 0) 0 else Random().nextInt(sides) + 1}
+val rollDice2 : (Int) -> Int = { sides -> if (sides == 0) 0 else Random().nextInt(sides) + 1}
+
+// called in main
+//gamePlay(rollDice2(4))
+
+fun gamePlay(diceRoll : Int) {
+    println(diceRoll)
+}
+
+// Kotlin Filters practice
+fun funWithSpices() {
+    val spices = listOf("curry", "pepper", "cayene", "ginger", "red curry", "green curry", "red pepper")
+    //Get spices containing curry and sorted by length
+    spices.filter{ it.contains("curry") }.sortedBy { it.length }
+    //Get spices starting with c and ending with e
+    spices.filter{ it.startsWith('c') }.filter{ it.endsWith('e') }
+    //Get first 3 spices and starting with c
+    spices.take(3).filter{ it.startsWith('c') }
 }
 
 fun whatShouldIDoToday(mood : String,
                        weather : String = "sunny",
                        temperature : Int = 24) : String
 {
+    fun sadMood (mood : String) = mood == "sad"
+    fun rainyWeather(weather : String) = weather == "rainy"
+    fun zeroTemp(temperature : Int) = temperature == 0
+    fun tempAbove35(temperature : Int) = temperature > 35
 
     return when {
         mood == "happy" && weather == "sunny" -> "Go for a walk"
+        sadMood(mood) && rainyWeather(weather) && zeroTemp(temperature) -> "Stay in bed"
+        tempAbove35(temperature) -> "Go Swimming"
         else -> "Stay home and read."
     }
 }
+
 
 fun getFortuneCookie(birthday : Int) : String {
     val fortunes = listOf("You will have a great day!",
@@ -71,6 +108,22 @@ fun shouldChangeWater(day : String,
                       temperature : Int = 22,
                       dirty : Int = 20) : Boolean {
     return true
+}
+
+// Lambda stuff
+var dirty = 20
+
+val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
+fun feedFish(dirty : Int) = dirty + 10
+
+fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
+    return operation(dirty)
+}
+
+fun dirtyProcessor() {
+    dirty = updateDirty(dirty, waterFilter)
+    dirty = updateDirty(dirty, ::feedFish)
+    dirty = updateDirty(dirty) { dirty -> dirty + 50 }
 }
 
 // Assuming tank has decorations and typical fish 2 inches long
